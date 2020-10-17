e32460ee7dfa0ba458f3f3f502c7cfa83e480467
kennedy.ikeka <kennedy.ikeka@ipigroupng.com>
2020-10-17 12:39:42 +0100

    first commit

---
+++ classes/Base.js
@@ -0,0 +1,33 @@
+const Components = require('./Components');
+const ColorPicker = require('./ColorPicker');
+const ArrayLibrary = require('./../functions/ArrayLibrary');
+const ObjectsLibrary = require('./../functions/ObjectsLibrary');
+const Icons = require('./../Icons');
+
+class Empty {
+}
+
+class Base extends Components {
+    constructor(theWindow = Empty) {
+        super(theWindow);
+        this.colorHandler = new ColorPicker();
+        this.array = ArrayLibrary();
+        this.object = ObjectsLibrary();
+        this.icons = Icons;
+
+        this.styles = [
+            'https://kade-95.github.io/kerdx/css/table.css',
+            'https://kade-95.github.io/kerdx/css/cell.css',
+            'https://kade-95.github.io/kerdx/css/form.css',
+            'https://kade-95.github.io/kerdx/css/picker.css',
+            'https://kade-95.github.io/kerdx/css/select.css',
+            'https://kade-95.github.io/kerdx/css/json.css',
+            'https://kade-95.github.io/kerdx/css/popup.css'
+        ];
+        for (let style of this.styles) {
+            this.loadCss(style);
+        }
+    }
+}
+
+module.exports = Base;
---
+++ index.js
@@ -0,0 +1,47 @@
+const Base = require('./classes/Base');
+const ColorPicker = require('./classes/ColorPicker');
+const Components = require('./classes/Components');
+const Func = require('./classes/Func');
+const JSElements = require('./classes/JSElements');
+const Matrix = require('./classes/Matrix');
+const NeuralNetwork = require('./classes/NeuralNetwork');
+const Period = require('./classes/Period');
+const Server = require('./classes/Server');
+const SessionsManager = require('./classes/SessionsManager');
+const Template = require('./classes/Template');
+const Tree = require('./classes/Tree');
+const AppLibrary = require('./functions/AppLibrary');
+const AnalysisLibrary = require('./functions/AnalysisLibrary');
+const ArrayLibrary = require('./functions/ArrayLibrary');
+const Compression = require('./functions/Compression');
+const MathsLibrary = require('./functions/MathsLibrary');
+const Shadow = require('./functions/Shadow');
+const ObjectsLibrary = require('./functions/ObjectsLibrary');
+const IndexedLibrary = require('./functions/IndexedLibrary');
+const MongoLibrary = require('./functions/MongoLibrary');
+const Icons = require('./Icons');
+
+module.exports = {
+    Base,
+    ColorPicker,
+    Period,
+    Matrix,
+    Func,
+    Components,
+    Template,
+    NeuralNetwork,
+    Icons,
+    Shadow,
+    ArrayLibrary,
+    ObjectsLibrary,
+    MathsLibrary,
+    AnalysisLibrary,
+    Compression,
+    Server,
+    SessionsManager,
+    AppLibrary,
+    IndexedLibrary,
+    MongoLibrary,
+    Tree,
+    JSElements
+}
